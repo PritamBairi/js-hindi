390bf2c609ef57bedf1dc37b9748b16110fd951f
PritamBairi <130812664+PritamBairi@users.noreply.github.com>
2023-06-30 05:52:32 +0000

    conversion of dataTypes

---
+++ 01_basic/03_datatypes_conversion.js
@@ -0,0 +1,22 @@
+let score = "33";
+
+console.log(typeof score);
+
+let valueInNumber = Number(score);
+
+console.log(typeof valueInNumber);
+
+let loggedIn = "";
+
+let booleanloggedIn = Boolean(loggedIn);
+
+console.log(booleanloggedIn);
+
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+
+console.log(stringNumber);
+
+console.log(typeof stringNumber);
