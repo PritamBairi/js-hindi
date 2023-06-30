c041dc139caf3f4701c42e87913dba09831667f5
PritamBairi <130812664+PritamBairi@users.noreply.github.com>
2023-06-30 06:46:45 +0000

    datatypes_conversions

--- 01_basic/03_datatypes_conversion.js
+++ 01_basic/03_datatypes_conversion.js
@@ -1,22 +1,36 @@
 let score = "33";
 
-console.log(typeof score);
+// console.log(typeof score);
 
 let valueInNumber = Number(score);
 
-console.log(typeof valueInNumber);
+// console.log(typeof valueInNumber);
 
 let loggedIn = "";
 
 let booleanloggedIn = Boolean(loggedIn);
 
-console.log(booleanloggedIn);
+// console.log(booleanloggedIn);
 
 
 let someNumber = 33;
 
 let stringNumber = String(someNumber);
 
-console.log(stringNumber);
+// console.log(stringNumber);
 
-console.log(typeof stringNumber);
+// console.log(typeof stringNumber);
+
+// console.table([2+2, 2-2, 2*2, 2**3, 2/3, 2%3]);
+
+
+let str1 = "Pritam";
+let str2 = " Bairi";
+
+console.log(str1 + str2);
+console.log(2 + 2 + "1");
+
+let gameCounter = 100;
+
+console.log(++gameCounter);
+// console.log(gameCounter++);
