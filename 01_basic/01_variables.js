0531df4b0c887f62ab84d3322251cbc943d5da25
PritamBairi <130812664+PritamBairi@users.noreply.github.com>
2023-06-30 05:20:00 +0000

    dataTypes

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144563;
+let accountEmail = "pritam2011@gmail.com"
+var accountPassword = "23434";
+accountCity = "Ghatal";
+let accountState;
+
+// accountId = 45354;
+accountEmail = "pritambairi7165@gmail.com";
+accountPassword = "75645";
+accountCity = "Kolkata";
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
