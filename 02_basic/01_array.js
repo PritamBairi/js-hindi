a417781b03211f4f1d2bee0206d12b502427b6f1
PritamBairi <130812664+PritamBairi@users.noreply.github.com>
2023-07-07 18:19:50 +0000

    array

---
+++ 02_basic/01_array.js
@@ -0,0 +1,28 @@
+const myArr = [1, 2, 3, 5, 9, 3];
+// console.log(myArray[3]);
+
+const myHeros = ["Iron Man", "Bat Man", "Super Man"];
+const myArr2 = new Array(1,3,2);
+// console.log(myArr2);
+
+// myArr.push(7);
+// console.log(myArr);
+// myArr.pop()
+// console.log(myArr);
+// myArr.unshift(8)
+// console.log(myArr);
+// myArr.shift();
+// console.log(myArr);
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+const newArr = myArr.join();
+// console.log(newArr);
+
+const myAr1 = myArr.slice(1,3);
+console.log(myAr1);
+console.log(myArr);
+const myAr2 = myArr.splice(1,3);
+console.log(myAr2);
+console.log(myArr);
