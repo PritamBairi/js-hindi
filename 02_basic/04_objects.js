3bce07dda4ac405996b02b2f8ab6cf380624154d
PritamBairi <130812664+PritamBairi@users.noreply.github.com>
2023-07-08 06:52:26 +0000

    objects

---
+++ 02_basic/04_objects.js
@@ -0,0 +1,37 @@
+// const tinderUser = new Object();  -----  Singelton object
+const tinderUser = {};           //  ------ non-Sinngelton object
+
+tinderUser.id = "Pritam123";
+tinderUser.name = "Pritam";
+tinderUser.isLoggedin = true;
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userFullname: {
+            firstName: "Pritam",
+            lastName: "Bairi"
+        }
+    }
+
+}
+
+console.log(regularUser.fullname.userFullname.firstName);
+
+
+const obj1 = {1: "a",2: "b"}
+const obj2 = {3: "c", 4:"d"}
+
+// const obj3 = Object.assign({}, obj1, obj2);
+
+const obj3 = {...obj1, ...obj2};
+
+// console.log(obj3);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty("id"));
